cd221a55fd1700598eed86cc007af2ac87528e5b
Dan Chung <danetix@gmail.com>
2016-08-29 13:54:43 -0400

    added support for marker types and marker time reset

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ map.setMaxBounds(bounds);
 $('.leaflet-container').css('cursor','crosshair');
 
 window.onload = function() {
-	//localStorage.setItem('markerData', "");
+	//localStorage.setItem('markerData', '');
 
 	markerData = getMarkers();
 	markerData = loadMarkers(markerData);
@@ -52,6 +52,13 @@ function onMapClick(e) {
 function onPopupOpen() {
 	var e = this;
 
+	// autofill saved data
+	if(markerData[e._leaflet_id]) {
+		$('input[name="markerHours' + e._leaflet_id + '"]').val(markerData[e._leaflet_id][2]);
+		$('input[name="markerMins' + e._leaflet_id + '"]').val(markerData[e._leaflet_id][3]);
+		$('input[name="markerType' + e._leaflet_id + '"][value="' + markerData[e._leaflet_id][5] + '"').prop('checked', true);
+	}
+
 	$('.deleteMarkerButton:visible').click(function() {
 		map.removeLayer(e);
 		onDelete(e);
@@ -60,6 +67,10 @@ function onPopupOpen() {
 	$('.saveMarkerButton:visible').click(function() {
 		onSave(e);
 	});
+
+	$('.resetMarkerButton:visible').click(function() {
+		onReset(e);
+	})
 }
 
 function onSave(e) {
@@ -68,49 +79,82 @@ function onSave(e) {
 	var markerHours = $('input[name="markerHours' + e._leaflet_id + '"]').val();
 	var markerMins = $('input[name="markerMins' + e._leaflet_id + '"]').val();
 	var startDate = Date.now();
+	var markerType = $('input[name="markerType' + e._leaflet_id + '"]:checked').val();
+	console.log(markerType);
 
 	// dont forget to change newMarkerObject
-	var newData = [e._leaflet_id, e._latlng, markerHours, markerMins, startDate];
+	var newData = [e._leaflet_id, e._latlng, markerHours, markerMins, startDate, markerType];
 	
 	markerData[e._leaflet_id] = newData;
 
 	storeMarkers(markerData);
 
-	e.setTooltipContent('00:00:00');
+	e.setTooltipContent('0:00:00');
 	updateTime(e._leaflet_id, markerHours, markerMins, startDate);
+
 	e.closePopup();
 }
 
 function onDelete(e) {
-	// var markerId = $('.marker-edit.' + this._leaflet_id);
-	// console.log(markerId);
 	delete markerData[e._leaflet_id];
 	storeMarkers(markerData);
 }
 
+function onReset(e) {
+	if(markerData[e._leaflet_id]) {
+		var markerHours = markerData[e._leaflet_id][2];
+		var markerMins = markerData[e._leaflet_id][3];
+		var startDate = Date.now();
+
+		var selectedMarkerType = markerData[e._leaflet_id][5];
+
+		if(selectedMarkerType == 'lootCrate') {
+			markerHours = '2';
+			markerMins = '00';
+		}
+		if(selectedMarkerType == 'uplinkBlockade') {
+			markerHours = '0';
+			markerMins = '45';
+		}
+
+		updateTime(e._leaflet_id, markerHours, markerMins, startDate);
+
+		markerData[e._leaflet_id][2] = markerHours;
+		markerData[e._leaflet_id][3] = markerMins;
+		markerData[e._leaflet_id][4] = startDate;
+
+		storeMarkers(markerData);
+		e.closePopup();
+	}
+}
+
 function loadMarkers(markerObject) {
 	var newMarkerObject = Object();
-	//console.log(markerObject);
+	console.log(markerObject);
 	for(var key in markerObject) {
 		var markerArray = markerObject[key];
 		var markerId = markerArray[0];
 		var markerHours = markerArray[2];
 		var markerMins = markerArray[3];
 		var startDate = markerArray[4];
+		var markerType = markerArray[5];
 		var latlng = new Object();
 		var popupContent = getPopupContent(markerId);
 
 		latlng = markerArray[1];
 		loadedMarker = new L.marker(latlng).addTo(map);
 		toolTipSettings.className = loadedMarker._leaflet_id;
-		loadedMarker.bindTooltip('00:00:00', toolTipSettings);
+		loadedMarker.bindTooltip('0:00:00', toolTipSettings);
+
+		$('input[name="markerType' + loadedMarker._leaflet_id + '"][value="' + markerType + '"').prop('checked', true);
+
 		loadedMarker.on('popupopen', onPopupOpen);
 		loadedMarker.bindPopup(popupContent, popupOptions);
 
 		updateTime(loadedMarker._leaflet_id, markerHours, markerMins, startDate);
 
 		// we have to save newly generated ids since we cant retain the old ids after loading the markers
-		newMarkerObject[loadedMarker._leaflet_id] = [loadedMarker._leaflet_id, loadedMarker._latlng, markerHours, markerMins, startDate];
+		newMarkerObject[loadedMarker._leaflet_id] = [loadedMarker._leaflet_id, loadedMarker._latlng, markerHours, markerMins, startDate, markerType];
 	}
 
 	return newMarkerObject;
@@ -121,6 +165,8 @@ function storeMarkers(a) {
 }
 
 function getMarkers() {
+	var a;
+
 	if(localStorage.getItem('markerData')) {
 		a = JSON.parse(localStorage.getItem('markerData'));
 	}
@@ -132,7 +178,7 @@ function updateTime(markerId, markerHours, markerMins, startDate) {
 	addToDate = ((markerHours * 60 * 60) + (markerMins * 60)) * 1000;
 
 	$('.leaflet-tooltip.' + markerId).countdown(startDate + addToDate, function(event) {
-		$(this).html(event.strftime('%H:%M:%S'));
+		$(this).html(event.strftime('%-H:%M:%S'));
 	});
 }
 
@@ -140,8 +186,8 @@ function getPopupContent(markerId) {
 	popupContent = "" +
 	"<div class='marker-box'>" +
 		"<div class='marker-edit " + markerId + "'>" +
-			"<p>Time <input type='text' name='markerHours" + markerId + "' class='markerTextBox' id='markerHours' maxlength='2' />:<input type='text' name='markerMins" + markerId + "' class='markerTextBox' id='markerMins' maxlength='2' /></p>" +
-			"<p><input type='radio' />Loot Crate<br /><input type='radio' />Uplink/Blockade</p>" +
+			"<p>Time <input type='text' name='markerHours" + markerId + "' class='markerTextBox' id='markerHours' maxlength='1' /> : <input type='text' name='markerMins" + markerId + "' class='markerTextBox' id='markerMins' maxlength='2' /></p>" +
+			"<p><input type='radio' name='markerType" + markerId + "' value='lootCrate' />Loot Crate<br /><input type='radio' name='markerType" + markerId + "' value='uplinkBlockade' />Uplink/Blockade<br /><input type='radio' name='markerType" + markerId + "' value='custom' />Custom</p>" +
 			"<a href='#' class='saveMarkerButton'>Save</a> | <a href='#' class='deleteMarkerButton'>Delete</a> | <a href='#' class='resetMarkerButton'>Reset</a>" +
 	"</div>";
 
